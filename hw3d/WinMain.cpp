80bae3c54b9777aef71b2b537e71057d79e30752
Ilya <arzaevilya@gmail.com>
2019-07-15 00:11:15 +0700

    2

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,12 +1,23 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch( msg )
+	{
+	case WM_CLOSE:
+		PostQuitMessage( 69 );
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow
 )
-
 
 {
 	const auto pClassName = "hw3dbutts";
@@ -33,7 +44,24 @@ int CALLBACK WinMain(
 		200, 200, 640, 480,
 		nullptr, nullptr, hInstance, nullptr
 	);
+	// show the window
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
-	return 0;
+
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while ( (gResult = GetMessage( &msg, nullptr, 0, 0 )) > 0 )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
