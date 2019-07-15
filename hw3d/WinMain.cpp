22cf60d75989f017eb4a61481e2fe689f889c9b3
Ilya <arzaevilya@gmail.com>
2019-07-15 19:49:52 +0700

    first fix

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,43 +1,5 @@
-#include <Windows.h>
-#include <iostream>
-#include <sstream>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch( msg )
-	{
-	case WM_CLOSE:
-		PostQuitMessage( 69 );
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'D') 
-		{
-			SetWindowText(hWnd, "Respects");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Wow");
-		}
-		break;
-	case WM_CHAR:
-		{
-			static std::string title;
-			title.push_back((char)wParam);
-			SetWindowText(hWnd, title.c_str());
-		}
-		break;
-	case WM_LBUTTONDOWN:
-	{
-		POINTS pt = MAKEPOINTS(lParam);
-		std::ostringstream oss;
-		oss << "(" << pt.x << "," << pt.y << ")";
-		SetWindowText(hWnd, oss.str().c_str());
-	}
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 
 int WinMain(
@@ -48,34 +10,10 @@ int WinMain(
 )
 
 {
-	const auto pClassName = "hw3dbutts";
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
 
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-	// show the window
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 300, "Box");
 
-	// message pump
+
 	MSG msg;
 	BOOL gResult;
 	while ( (gResult = GetMessage( &msg, nullptr, 0, 0 )) > 0 )
@@ -84,7 +22,6 @@ int WinMain(
 		DispatchMessage( &msg );
 	}
 
-	std::cout << gResult << std::endl;
 	if (gResult == -1)
 	{
 		return -1;
